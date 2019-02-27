f46023114d4d6baf94c9ab210d1228338a7988ef
Jack Xin <810057256@qq.com>
2019-02-27 15:11:31 +0800

    no message

---
+++ client/src/components/Admin/Home/Default.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <el-card>
+            欢迎您进入后台系统
+        </el-card>
+        <el-card>
+            <div align="center">
+                <el-button>退出系统</el-button>
+            </div>
+        </el-card>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return {
+            username:'辛浩然',
+            count:{
+                available:0,
+                topay:0,
+                ready:0,
+                ended:0
+            }
+        }
+    }
+}
+</script>
+<style>
+.el-card{
+    margin-bottom: 10px;
+}
+.number{
+    font-size:25px;
+    font-weight: bold;
+    color:#0f7fd2;
+}
+</style>
+
---
+++ client/src/components/Admin/Home/Subjects.vue
@@ -0,0 +1,5 @@
+<template>
+    <div>
+        科目管理
+    </div>
+</template>
